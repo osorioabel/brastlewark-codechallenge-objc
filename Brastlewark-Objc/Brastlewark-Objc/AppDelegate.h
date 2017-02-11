8e62100e8581535e2c17367ef67478d166f43780
Abel Osorio <abel@kogimobile.com>
2017-02-11 14:35:54 -0500

    Initial Project

---
+++ Brastlewark-Objc/Brastlewark-Objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Brastlewark-Objc
+//
+//  Created by Abel Osorio on 2/11/17.
+//  Copyright © 2017 Abel Osorio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
